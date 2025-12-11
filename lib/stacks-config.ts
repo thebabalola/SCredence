98c365ebe8c19b232f7e5a8710c47a909c0f02e0
thebabalola <t.babalolajoseph@gmail.com>
2025-12-11 11:36:55 +0100

    chore: add stacks app config constants

---
+++ lib/stacks-config.ts
@@ -0,0 +1,7 @@
+export const STACKS_APP_DETAILS = {
+  name: "StacksLend",
+  icon: "/favicon.ico",
+};
+
+export const STACKS_REDIRECT_PATH = "/";
+export const STACKS_MANIFEST_PATH = "/manifest.json";
